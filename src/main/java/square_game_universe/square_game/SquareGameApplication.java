0c6651919862ea209393f96cc5c5f5e00f55ea96
geraldine.rouy <geraldine.rouy@le-campus-numerique.fr>
2024-12-13 13:48:10 +0100

    First commit

---
+++ src/main/java/square_game_universe/square_game/SquareGameApplication.java
@@ -0,0 +1,13 @@
+package square_game_universe.square_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquareGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquareGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/square_game_universe/square_game/SquareGameApplicationTests.java
@@ -0,0 +1,13 @@
+package square_game_universe.square_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquareGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
