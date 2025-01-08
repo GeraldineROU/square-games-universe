909c5b5b43c16a01b8996988c8fba7c3ed787c8e
geraldine <geraldine.rouy@le-campus-numerique.fr>
2025-01-08 15:09:23 +0100

    working on step 4 of iteration 3 : H2 implementation

--- src/main/java/square_game_universe/square_game/player/PlayerController.java
+++ src/main/java/square_game_universe/square_game/player/PlayerController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/players")
@@ -19,23 +20,23 @@ public class PlayerController {
     }
 
     @GetMapping("/{id}")
-    public PlayerJPAEntity getPlayerById(@PathVariable("id") int id) {
-        return playerJPARepository.getReferenceById(id);
+    public Optional<PlayerJPAEntity> getPlayerById(@PathVariable("id") Integer id) {
+        return playerJPARepository.findById(id);
     }
 
-//    @PostMapping()
-//    public Integer createPlayer(@RequestBody PlayerDTO playerDTO) {
-//        return playerService.create(playerDTO);
-//    }
+    @PostMapping()
+    public PlayerJPAEntity createPlayer(@RequestBody PlayerJPAEntity playerJPAEntity) {
+        return playerJPARepository.save(playerJPAEntity);
+    }
 
-//    @DeleteMapping("/{id}")
-//    public Collection<PlayerDTO> deletePlayerById(@PathVariable("id") int id) {
-//        return playerService.deleteById(id);
-//    }
+    @DeleteMapping("/{id}")
+    public void  deletePlayerById(@PathVariable("id") Integer id) {
+        playerJPARepository.deleteById(id);
+    }
 
-//    @PutMapping("/{id}")
-//    public Optional<PlayerDTO> editPlayer(@RequestBody PlayerDTO playerDTO) {
-//        return playerService.edit(playerDTO.id(), playerDTO.name());
-//    }
+    @PutMapping("/{id}")
+    public void editPlayer(@RequestBody PlayerJPAEntity playerJPAEntity) {
+        playerJPARepository.save(playerJPAEntity);
+    }
 
 }
